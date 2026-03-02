074a123da24e4ad1d9c1d1187c5680bdcbfda2e4
john <afariogunjohn2002@gmail.com>
2026-03-02 08:44:43 +0100

    fix: allowed access

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -43,6 +43,14 @@ resource "aws_security_group" "k8s_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  description = "K8s NodePorts"
+  from_port   = 30000
+  to_port     = 32767
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   egress {
     from_port   = 0
     to_port     = 0
